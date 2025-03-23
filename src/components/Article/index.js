75e5fb88d3703c4d0cab3caf352248d2bfc81313
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-03-23 01:43:04 -0300

    aula 11

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,11 +1,15 @@
 import Post from "../Post";
 
-const Article = ()=> {
-    return(
-        <>
-        <h1>Article</h1>
-        <Post/>
-        </>
+const Article = () => {
+    return (
+        <section className="container">
+            <h1>Article</h1>
+            <div className="mt-5 container-posts">
+                <Post />
+                <Post />
+                <Post />
+            </div>
+        </section>
     );
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,26 @@
 
 
 const Post = () => {
-    return(
-        <h2>Post aqui</h2>
+    return (
+        <div className="post mx-2">
+            <div className="img-post"></div>
+            <h5>Tecnologia</h5>
+            <h3>O guia definitivo sobre o blog</h3>
+            <p>Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit.
+                Providentquae maiores consectetur
+                quia animi?Nesciunt iure perferendis
+                reprehenderit quae,totam temporibus
+                incidunt quo nisi eius sit
+                necessitatibus, sunt sint qui.</p>
+            <div className="flex pt-2">
+                <div className="img-profile"></div>
+                <div className="desc-profile ml-2">
+                    <h6 className="color-blue">Fulano de tal</h6>
+                <p>March 23, 2025 - 8 min read</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
