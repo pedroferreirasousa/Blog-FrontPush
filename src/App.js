2e76341b6b0e259cc476cabd03d3e71d0121d40e
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-03-23 04:09:50 -0300

    aula 15

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import Article from "./components/Article";
 import Header from "./components/Header";
 
+import {useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Article/>
-    </div>
-  );
-}
+
+  let [carrinho, setCarrinho] = useState(0);
+
+
+
+  const AddItem = () => {
+    setCarrinho(carrinho + 1)
+    console.log(carrinho)
+  }
+
+  const RemoveItem = () => {
+    if (carrinho > 0) {
+      setCarrinho(carrinho - 1)
+      console.log(carrinho)
+    }
+  }
+
+    return (
+      <div className="App">
+        <Header />
+        <Article />
+
+        <div className="btndiv flex-space-between">
+          <h1>Carrinho de compras: {carrinho}</h1>
+          <div className="btns flex">
+            <button className="add-button" onClick={() => AddItem()}>ADICIONAR</button> <br />
+            <button className="remove-button" onClick={() => RemoveItem()}>REMOVER</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
 export default App;
